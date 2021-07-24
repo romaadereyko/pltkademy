63e9b4bb04a42449a8f640eea4c5af3644afdd34
Roman Adereyko <romaadereyko@list.ru>
2021-07-24 12:58:23 +0300

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -2,7 +2,7 @@
 
 for (( i = 1; i <= 10; i++))
 do
-touch /root/playtika/$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13 ; echo '').txt
+touch /root/playtika/$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 1000 ; echo '').txt
 done
 
 exit 0
