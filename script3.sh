c64b7b965d379266cc024335986c56acfbbd71bd
Roman Adereyko <romaadereyko@list.ru>
2021-07-24 13:06:11 +0300

    Update script3.sh

--- script3.sh
+++ script3.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
 
-total_memory=$(free | grep Mem | awk {'print $2'})
-echo "Total memory = $total_memory" > /root/playtika/SRV_info.txt
+echo "Total memory = $(free | grep Mem | awk {'print $2'})" > /root/playtika/SRV_info.txt
 
-total_disk=$(fdisk -l | head -n 1 | awk {'print $3 $4'})
-echo "Total disk = $total_disk" >> /root/playtika/SRV_info.txt
+echo "Total disk = $(fdisk -l | head -n 1 | awk {'print $3 $4'})" >> /root/playtika/SRV_info.txt
 
-total_cpu_cores=$(cat /proc/cpuinfo | grep processor | wc | awk {'print $1'})
-echo "Total cpu cores = $total_cpu_cores" >> /root/playtika/SRV_info.txt
+echo "Total cpu cores = $(cat /proc/cpuinfo | grep processor | wc | awk {'print $1'})" >> /root/playtika/SRV_info.txt
 
-name=$(hostname)
-echo "Server name is $name" >> /root/playtika/SRV_info.txt
-
+echo "Server name is $(hostname)" >> /root/playtika/SRV_info.txt
 
 exit 0
